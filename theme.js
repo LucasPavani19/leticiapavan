6b3c3fd75e8a6909d35d56cd3479667035134f70
lucaspavani19 <lucaspavani132@gmail.com>
2025-02-27 22:23:36 -0300

    Desabilitando botão de modo noturno

--- theme.js
+++ theme.js
@@ -1,3 +1,11 @@
+// theme.js
+
+/* 
+ * Este script é responsável por gerenciar o tema do site.
+ * Ele adiciona um evento de mudança ao switch de tema, que
+ * altera o tema do site entre claro e escuro.
+ */
+
 // Verificar se há preferência salva
 const savedTheme = localStorage.getItem('theme') || 'dark';
 document.documentElement.setAttribute('data-theme', savedTheme);
